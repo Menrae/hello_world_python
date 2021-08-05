0423c88e10b19a91ac98992f61b88522aca2442d
aashasti@gmail.com <aashasti@gmail.com>
2021-08-05 12:46:12 -0700

    python array and vector tests

--- hello_world_python/hello_world_python.py
+++ hello_world_python/hello_world_python.py
@@ -9,3 +9,32 @@ def sum(n):
 print(sum(5));
 print(sum(15));
 print(sum(151));
+
+
+def array_test():
+    ar = [3, 2, 4, 5]
+
+    ar.pop()
+
+    ar.append(6)
+
+    print(ar)
+    print("Index of 4: ", ar.index(4)) # index of given value
+
+    ar.remove(4) # remove the first occurence of the item with the given value
+    print("Removed 4: ", ar)
+
+    ar.reverse()
+    print("reversed: ", ar)
+    print("sorted return: ", sorted(ar))
+
+    ar.sort()
+    print("sorted in place: ", ar)
+
+
+def main():
+    array_test()
+
+
+if __name__ == "__main__":
+    main()
